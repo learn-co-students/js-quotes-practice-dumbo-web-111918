366ccb41e9c0eeba2cd23c5d3b6f1c0e67fa8689
jriv95 <joel.rivera7995@gmail.com>
2019-01-17 16:45:50 -0500

    Done a little differently

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 // only runs after the DOM has finshed loading.
 
 document.addEventListener('DOMContentLoaded', e => {
+
   const quoteList = document.querySelector('#quote-list')
   const newQuoteForm = document.querySelector('#new-quote-form')
 
@@ -9,49 +10,36 @@ document.addEventListener('DOMContentLoaded', e => {
   .then(res => res.json())
   .then(quotes => quotes.forEach(quote => showQuote(quote)))
 
+
   const showQuote = (quote) => {
-    quoteList.innerHTML += `<li class='quote-card' id="quote-${quote.id}" data-id="${quote.id}">
-    <blockquote class="blockquote">
-    <p class="mb-0">${quote.quote}</p>
-    <footer class="blockquote-footer">${quote.author}</footer>
-    <br>
-    <button class='btn-success'>Likes: <span>${quote.likes}</span></button>
-    <button class='btn-danger' data-id="${quote.id}">Delete</button>
-    </blockquote>
+    quoteList.innerHTML += `<li class='quote-card' id="data-${quote.id}" data-id="${quote.id}">
+      <blockquote class="blockquote">
+        <p class="mb-0">${quote.quote}</p>
+        <footer class="blockquote-footer">${quote.author}</footer>
+        <br>
+        <button class='btn-success' data-id="${quote.id}" >Likes: <span>${quote.likes}</span></button>
+        <button class='btn-danger' data-id="${quote.id}">Delete</button>
+      </blockquote>
     </li>`
   }
-
-  quoteList.addEventListener('click', e => {
-    if(e.target.classList.contains('btn-danger')) {
-      // const quoteId = e.target.parentNode.parentNode.dataset.id; //('data-id')
-      const quoteId = e.target.dataset.id
-
-      fetch(`http://localhost:3000/quotes/${quoteId}`, {
-        method: 'DELETE'
-      })
-      .then(res => res.json())
-      .then(quote => document.querySelector(`#quote-${quoteId}`).remove())
-    }
-  })
 
   newQuoteForm.addEventListener('submit', e => {
     e.preventDefault()
     const newQuote = document.querySelector('#new-quote').value
     const author = document.querySelector('#author').value
 
-    newData = {
-      quote: newQuote,
-      author: author,
-      likes: 0
+    const quoteData = {
+        quote: newQuote,
+        author: author,
+        likes: 0
     }
 
-    fetch('http://localhost:3000/quotes', {
+    fetch(`http://localhost:3000/quotes`, {
       method: 'POST',
-      body: JSON.stringify(newData),
+      body: JSON.stringify(quoteData),
       headers: {
         'Content-Type': 'application/json'
       }
-
     })
     .then(res => res.json())
     .then(quote => showQuote(quote))
@@ -59,17 +47,29 @@ document.addEventListener('DOMContentLoaded', e => {
   })
 
   quoteList.addEventListener('click', e => {
-    if(e.target.classList.contains('btn-success')) {
-      const quoteId = e.target.parentNode.parentNode.getAttribute('data-id')
-      const span = e.target.parentNode.querySelector('span')
-      const spanValue = span.innerText
-      const likes = parseInt(spanValue)
+    if(e.target.classList.contains('btn-danger')) {
+      const dataId = e.target.dataset.id
 
+      fetch(`http://localhost:3000/quotes/${dataId}`, {
+        method: 'DELETE'
+      })
+      .then(res => res.json())
+      .then(quote => document.querySelector(`#data-${dataId}`).remove())
 
-      fetch(`http://localhost:3000/quotes/${quoteId}`, {
+    }
+  })
+
+   quoteList.addEventListener('click', e => {
+     if (e.target.classList.contains('btn-success')) {
+      const dataId = e.target.dataset.id
+      const span = e.target.querySelector('span')
+      const spanInfo = span.innerText
+      const numOfLikes = parseInt(spanInfo)
+
+      fetch(`http://localhost:3000/quotes/${dataId}`, {
         method: 'PATCH',
         body: JSON.stringify({
-          likes: likes + 1
+          likes: numOfLikes + 1
         }),
         headers: {
           'Content-Type': 'application/json'
@@ -82,4 +82,4 @@ document.addEventListener('DOMContentLoaded', e => {
   })
 
 
-})
+}) // DOMContentLoaded
